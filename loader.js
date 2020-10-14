44421139d54719b811f5505b7e597dacc8786684
joshwilson00 <joshuawilson1605@gmail.com>
2020-10-14 19:20:49 +0100

    Now does a restaurant, its menus and items all in order

--- loader.js
+++ loader.js
@@ -9,29 +9,27 @@ function insertRestaurant(restaurants, callback){
         if (err) throw new Error(err);
         const restaurant_id = this.lastID;
         console.log(`Restautant added with ID: ${restaurant_id}`);
-        insertMenus(restaurant, restaurant_id);
-        insertRestaurant(restaurants, callback);
+        insertMenus(restaurants, restaurant, restaurant_id, callback);
     });
 }
 
-function insertMenus(restaurant, restaurant_id){
+function insertMenus(restaurants, restaurant, restaurant_id, callback){
     console.log('Inserting menu for this restaurant: ', restaurant);
-    if (!restaurant.menus.length) return;
+    if (!restaurant.menus.length) return insertRestaurant(restaurants, callback);
     const menu = restaurant.menus.pop();
     db.run(`INSERT INTO menus(title, restaurant_id) VALUES(?, ?);`, [menu.title, restaurant_id], function(err){
         if (err) throw new Error (err);
         const menu_id = this.lastID;
-        insertItem(menu, menu_id);
-        insertMenus(restaurant, restaurant_id);
+        insertItem(restaurants, restaurant, restaurant_id, menu, menu_id, callback);
     });
 }
-function insertItem(menu, menu_id){
+function insertItem(restaurants, restaurant, restaurant_id, menu, menu_id, callback){
     console.log('Inserting item for this menu: ', menu);
-    if (!menu.items.length) return;
+    if (!menu.items.length) return insertMenus(restaurants, restaurant, restaurant_id, callback);
     const item = menu.items.pop();
     db.run(`INSERT INTO items(name, price, menu_id) VALUES(?, ?, ?);`, [item.name, item.price, menu_id], function(err){
         if (err) throw new Error (err);
-        insertItem(menu, menu_id);
+        insertItem(restaurants, restaurant, restaurant_id, menu, menu_id, callback);
     });  
 }
 
